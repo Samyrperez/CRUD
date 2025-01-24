4ea7d2f50cc5411514d45524697213d3b7a7eda2
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-01-24 11:37:01 -0500

    create php

--- create.php
+++ create.php
@@ -1,3 +1,28 @@
 <?php
 
+include "db.php";
 
+// capturamos los datos del fomr saveForm
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $nombre = $_POST['nombre'];
+    $estado = $_POST['estado'];
+    $fecha_actual = date('Y-m-d H:i:s');
+
+    // construimos la consulta
+
+    if(!empty($nombre) && !empty($estado)){
+        $sql = "INSERT INTO datos_usuarios (nombre, estado, fecha) VALUES ('$nombre', '$estado', '$fecha_actual')";
+
+        if ($conn->query($sql) === TRUE){
+            echo true;
+        } else {
+            echo false;
+        }
+    } else {
+        echo false;
+    }
+
+} else {
+    echo false;
+}
