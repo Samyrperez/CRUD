1a50cc44484b17bfde071d48ca95a44b681c339a
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-01-24 14:59:55 -0500

    read php

---
+++ read.php
@@ -0,0 +1,37 @@
+<?php
+
+include "db.php";
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : null;
+$nombre = isset($_GET['nombre']) ? $conn->real_escape_string($_GET['id']) : null;
+
+$sql = "SELECT * FROM datos_usuarios EHERE 1";
+
+if ($id){
+    $sql .= "AND id = $id ";
+} 
+if ($nombre){
+    $sql .= "AND nombre LIKE '%$nombre%'";
+}
+
+$result = $con->query($sql);
+
+
+if ($result && $result->num_rows > 0)
+{
+    while ($row = $result->fetch_assoc())
+    {
+        echo "<tr>
+            <td id='id_{$row['id']}'>{$row['id']}</td>
+            <td id='nombre_{$row['id']}'>{$row['id']}</td>
+            <td id='estado_{$row['id']}'>{$row['id']}</td>
+            <td>
+                <button class='btn-edit' data-id='{$row['id']}'><img src='./image/edit.png'></button>
+                <button class='btn-delete' data-id='{$row['id']}'><img src='./image/delete.png'></button>
+            </td>
+            </tr>";
+    }
+} else {
+    echo '<tr><td colspan="4" class="texto-centrado">No hay datos disponibles</td></tr>';
+}
+$conn->close();
